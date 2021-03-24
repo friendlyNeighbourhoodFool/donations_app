c2a81dd81d68483508939cf13d060b9c4bb27b8d
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-03-24 19:42:33 +0530

    Add files via upload

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -24,19 +24,19 @@ class Product with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> toggleFavoriteStatus() async {
+  Future<void> toggleFavoriteStatus(String token, String userId) async {
     final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
     final url =
-        'https://myshop-grpproject-default-rtdb.firebaseio.com/products/$id.json';
+        'https://myshop-grpproject-default-rtdb.firebaseio.com/userFavorites/$userId/$id.json?auth=$token';
 
     try {
-      final response = await http.patch(
+      final response = await http.put(
         Uri.parse(url),
-        body: json.encode({
-          'isFavorite': isFavorite,
-        }),
+        body: json.encode(
+          isFavorite,
+        ),
       );
       if (response.statusCode >= 400) {
         _setFavValue(oldStatus);
